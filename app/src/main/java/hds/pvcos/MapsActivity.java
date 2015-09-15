8e8a1a6d3e89390ed98a4a7d0e5844504d78d9aa
Mikkel Kettunen <mikkel.kettunen@gmail.com>
2015-09-15 20:09:28 +0200

    Added location in mapfragment

--- app/src/main/java/hds/pvcos/MapsActivity.java
+++ app/src/main/java/hds/pvcos/MapsActivity.java
@@ -1,8 +1,10 @@
 package hds.pvcos;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -60,6 +62,6 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
     }
 }
